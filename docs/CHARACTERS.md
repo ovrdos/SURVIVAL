0779aec079b1fee7b0cb033c5d5914a4c574b154
kamal.hakim <hakim@coperbyte.com>
2026-09-06 09:16:52 -0700

    Adding postal sample files

---
+++ docs/CHARACTERS.md
@@ -0,0 +1,17 @@
+SURVIVAL CHARACTERS 
+
+bot01
+bot02
+bot03
+dcworker1
+dcworker2
+guard
+janitor
+fed01
+fed02
+dogbot01
+dogbot02
+healthbot01
+healthbot02
+hacker01
+hacker02
---
+++ scripts/list.sh
@@ -0,0 +1,4 @@
+grep -Rni "SOP" . --include="*.cpp" --include="*.h" >> list_data
+grep -Rni "MESH" . --include="*.cpp" --include="*.h" >> list_data
+grep -Rni "bounds" . --include="*.cpp" --include="*.h" >> list_data
+grep -Rni "RFile" . --include="*.cpp" --include="*.h" >> list_data
