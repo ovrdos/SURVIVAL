7eabfb4e2d32738d1891c443e1c3b1269a463989
kamal.hakim <hakim@coperbyte.com>
2026-09-06 09:37:13 -0700

    adding sample sourcecode

--- list.sh
+++
@@ -1,4 +0,0 @@
-grep -Rni "SOP" . --include="*.cpp" --include="*.h" >> list_data
-grep -Rni "MESH" . --include="*.cpp" --include="*.h" >> list_data
-grep -Rni "bounds" . --include="*.cpp" --include="*.h" >> list_data
-grep -Rni "RFile" . --include="*.cpp" --include="*.h" >> list_data
